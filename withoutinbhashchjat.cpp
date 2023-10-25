17d13056687dcf5ec2f28cdc23b6fcd41180142a
Keshav Sharma <140502712+Keshav2305@users.noreply.github.com>
2023-10-25 23:43:46 +0530

    Update withoutinbhashchjat.cpp

--- withoutinbhashchjat.cpp
+++ withoutinbhashchjat.cpp
@@ -2,10 +2,11 @@
 #include <string>
 #include <vector>
 #include <sstream>
+using namespace std;
 
 // Define a simple hash table entry to store word-frequency pairs.
 struct HashEntry {
-    std::string word;
+    string word;
     int frequency;
 
     HashEntry(const std::string& w) : word(w), frequency(1) {}
@@ -14,11 +15,11 @@ struct HashEntry {
 // Define a custom hash table class with basic operations.
 class HashTable {
     public:
-    std::vector<HashEntry> table;
+    vector<HashEntry> table;
     size_t size;
 
     // Custom hash function (simple example for demonstration purposes).
-    size_t customHash(const std::string& key) {
+    size_t customHash(const string& key) {
         size_t hash = 0;
         for (char c : key) {
             hash = (hash * 31 + static_cast<size_t>(c)) % size;
@@ -27,7 +28,7 @@ class HashTable {
     }
 
 
-    HashTable(size_t tableSize) : size(tableSize), table(tableSize) {}
+   HashTable(size_t tableSize) : size(tableSize), table(tableSize, HashEntry("")) {}
 
     // Custom insert operation.
     void insert(const std::string& key) {
@@ -61,11 +62,11 @@ class HashTable {
 
 int main() {
     // Input text (you can replace this with your own text).
-    std::string text = "This is a simple word frequency counter. It counts the frequency of each word in a given input text. This is a simple example.";
+    string text = "This is a simple word frequency counter. It counts the frequency of each word in a given input text. This is a simple example.";
 
     // Tokenize the input text.
-    std::string word;
-    std::istringstream stream(text);
+    string word;
+    istringstream stream(text);
     HashTable wordFrequency(100); // Adjust the table size as needed.
 
     while (stream >> word) {
@@ -83,11 +84,11 @@ int main() {
     }
 
     // Print word frequencies.
-    for (size_t i = 0; i < 100; i++) { // Adjust the loop range to match the table size.
-        if (!wordFrequency.table[i].word.empty()) {
-            std::cout << wordFrequency.table[i].word << ": " << wordFrequency.table[i].frequency << std::endl;
-        }
+    for (size_t i = 0; i < wordFrequency.size; i++) {
+    if (!wordFrequency.table[i].word.empty()) {
+        cout << wordFrequency.table[i].word << ": " << wordFrequency.table[i].frequency << std::endl;
     }
+}
 
     return 0;
 }
